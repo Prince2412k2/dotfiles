abe9aba2cdc53f6b5852f3f119e440d4649bc0aa
Your Name <you@example.com>
2025-08-22 16:11:16 +0530

    colorscheme conflict solved

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,29 +1,24 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin",
-    flavour = "mocha",
-    transparent = true,
+  -- Set rose-pine as the colorscheme in LazyVim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+
+  -- Configure rose-pine theme options
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "moon", -- options: 'main' | 'moon' | 'dawn'
+      dark_variant = "moon",
+      bold_vert_split = false,
+      dim_nc_background = true,
+      disable_background = true, -- transparency
+      disable_float_background = true,
+      disable_italics = false,
+    },
   },
 }
-
--- return {
---   "catppuccin/neovim",
---   name = "catppuccin",
---   config = function()
---     require("catppuccin").setup({
---       flavour = "mocha",
---       disable_background = true,
---       styles = {
---         italic = false,
---       },
---     })
---   --   end,
---   }
-
--- return {
---   "LazyVim/LazyVim",
---   opts = {
---     colorscheme = "rose-pine",
---   },
--- }
