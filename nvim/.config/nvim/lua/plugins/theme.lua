e65a54ad10569cf8e1b45dcd293deab4fba7233e
Your Name <you@example.com>
2025-08-22 16:38:04 +0530

    added a auto aliasing of tmux path to 'th'

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,24 +1,8 @@
 return {
-  -- Set rose-pine as the colorscheme in LazyVim
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
-
-  -- Configure rose-pine theme options
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      variant = "moon", -- options: 'main' | 'moon' | 'dawn'
-      dark_variant = "moon",
-      bold_vert_split = false,
-      dim_nc_background = true,
-      disable_background = true, -- transparency
-      disable_float_background = true,
-      disable_italics = false,
-    },
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin",
+    flavour = "mocha",
+    transparent = true,
   },
 }
